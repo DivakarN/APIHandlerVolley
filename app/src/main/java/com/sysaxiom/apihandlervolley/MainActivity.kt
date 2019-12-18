85688bd5a4fc067f987d6725e5af1e0b2d658503
Divakar <divakar.nandagopal@tpfsoftware.com>
2019-12-18 13:31:40 +0530

    Initial Commit

--- app/src/main/java/com/sysaxiom/apihandlervolley/MainActivity.kt
+++ app/src/main/java/com/sysaxiom/apihandlervolley/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         getInterest()
-        
+
         val jsonObject = JSONObject()
         jsonObject.put(ApiConstants.postApiJsonName, ApiConstants.postApiJsonValue)
         updateInterest(jsonObject.toString())
